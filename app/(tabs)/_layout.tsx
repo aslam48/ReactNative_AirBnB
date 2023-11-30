01ce898258baa828614f9f7f7ea8d0fb9734ca0b
aslam48 <youngfizzy001@gmail.com>
2023-11-30 22:30:03 +0100

    working

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const layout = () => {
+  return  <Tabs></Tabs>
+}
+
+export default layout
+
+const styles = StyleSheet.create({})
